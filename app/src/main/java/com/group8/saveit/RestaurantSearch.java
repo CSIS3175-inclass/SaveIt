b90b94d47e65b1e3c7408b3fa27552871a5dd5f1
FitiaHarena <randrianjafyfitiaharena@gmail.com>
2023-04-04 13:22:47 -0700

    Update RestaurantSearch to get restaurantId for restaurant activity

--- app/src/main/java/com/group8/saveit/RestaurantSearch.java
+++ app/src/main/java/com/group8/saveit/RestaurantSearch.java
@@ -3,20 +3,36 @@ package com.group8.saveit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
-public class RestaurantSearch extends AppCompatActivity {
+import java.util.ArrayList;
 
-    String arr1[] = {"res1","res2","res3","res4"};
-    int arr2[] = {R.drawable.pic,R.drawable.pic,R.drawable.pic,R.drawable.pic};
+public class RestaurantSearch extends AppCompatActivity {
+    ArrayList<Restaurant> restaurants=new ArrayList<>();
+    DatabaseHelper databaseHelper;
+//    String arr1[] = {"res1","res2","res3","res4"};
+//    int arr2[] = {R.drawable.pic,R.drawable.pic,R.drawable.pic,R.drawable.pic};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_search);
         ListView listView = findViewById(R.id.listviewRestraunt);
-        RestrauntSearchAdapter adapter =new RestrauntSearchAdapter(getApplicationContext(),arr1,arr2);
+
+        // TODO: 4/4/2023 Retrieve restaurant list based on customer's address city or city clicked
+        databaseHelper = new DatabaseHelper(this);
+        restaurants=databaseHelper.getAllRestaurants(); //replace when done
+
+        String[] arr1 = new String[restaurants.size()];
+        int[] arr2 = new int[restaurants.size()];
+        for(int i = 0; i<restaurants.size();i++){
+            arr1[i]=restaurants.get(i).getName();
+            arr2[i]=R.drawable.pic;
+        }
+
+        RestaurantSearchAdapter adapter =new RestaurantSearchAdapter(getApplicationContext(),arr1,arr2,restaurants);
         listView.setAdapter(adapter);
 
         ImageButton home = findViewById(R.id.home_icon);
