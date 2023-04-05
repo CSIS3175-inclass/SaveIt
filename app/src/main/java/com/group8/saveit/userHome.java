be4ef16abdaa338d179ee7f4938b058ba8875fe3
melvin sunny <melvinsunny098@gmail.com>
2023-04-04 23:46:34 -0700

    commit 3 displayed data

--- app/src/main/java/com/group8/saveit/userHome.java
+++ app/src/main/java/com/group8/saveit/userHome.java
@@ -16,14 +16,30 @@ public class userHome extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
         Intent intent=getIntent();
         TextView main_Name=findViewById(R.id.txtName_P);
+        TextView name=findViewById(R.id.Up_Name);
+        TextView phone=findViewById(R.id.textView4);
+TextView email=findViewById(R.id.Up_email);
+        EditText password=findViewById(R.id.Up_password);
+        TextView street=findViewById(R.id.Up_street);
+        TextView city=findViewById(R.id.Up_city);
+        TextView zip=findViewById(R.id.Up_zip);
+
+
         if(intent!= null) {
           String value = intent.getStringExtra("customerEmail");
 
 
-            //dbh = new DatabaseHelper(this);
+            dbh = new DatabaseHelper(this);
 
-          //  Customer customer = dbh.getCustomerByEmail(value);
-            main_Name.setText(value);
+          Customer customer = dbh.getCustomerByEmail(value);
+            main_Name.setText(customer.getName());
+            name.setText(customer.getName());
+         phone.setText(customer.getPhone());
+            email.setText(customer.getEmail());
+password.setText(customer.getPassword());
+           street.setText(customer.getStreetName());
+           city.setText(customer.getCity());
+           zip.setText(customer.getPostalCode());
 
         }
     }
